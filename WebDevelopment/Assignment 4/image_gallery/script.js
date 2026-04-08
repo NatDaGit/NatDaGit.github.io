da1348a999e8f2f8dc72aa5942b2b64f608656d3
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-07 22:02:48 -0400

    early progress
    Got the images added to a folder, and added some simple starter parts.

--- WebDevelopment/Assignment 4/image_gallery/script.js
+++ WebDevelopment/Assignment 4/image_gallery/script.js
@@ -6,4 +6,8 @@ const overlay = document.querySelector(".overlay");
 
 const images = [
 {filename: "pic1.jpg", alt: "Closeup of a human eye" }
+{filename: "pic2.jpg", alt: "Rock that looks like a Wave" }
+{filename: "pic3.jpg", alt: "Purple and White pansies" }
+{filename: "pic4.jpg", alt: "Secton of wall from a tomb" }
+{filename: "pic5.jpg", alt: "Large moth on a leaf" }
 ]
