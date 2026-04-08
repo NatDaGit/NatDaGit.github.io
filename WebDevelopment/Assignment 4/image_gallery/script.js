c1374ac7abca6fb027cf2d94f2b53f0e341f0486
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-07 22:50:01 -0400

    Progress update 2 (and 3)
    Realized i didnt need the image folder, and made progress in the image loop
    
    additionally forgot to press commit for progress update 2 -.- rough progress but progress regardless

--- WebDevelopment/Assignment 4/image_gallery/script.js
+++ WebDevelopment/Assignment 4/image_gallery/script.js
@@ -5,9 +5,31 @@ const btn = document.querySelector("button");
 const overlay = document.querySelector(".overlay");
 
 const images = [
-{filename: "pic1.jpg", alt: "Closeup of a human eye" }
-{filename: "pic2.jpg", alt: "Rock that looks like a Wave" }
-{filename: "pic3.jpg", alt: "Purple and White pansies" }
-{filename: "pic4.jpg", alt: "Secton of wall from a tomb" }
-{filename: "pic5.jpg", alt: "Large moth on a leaf" }
-]
+{filename: "pic1.jpg", alt: "Closeup of a human eye" },
+{filename: "pic2.jpg", alt: "Rock that looks like a Wave" },
+{filename: "pic3.jpg", alt: "Purple and White pansies" },
+{filename: "pic4.jpg", alt: "Secton of wall from a tomb" },
+{filename: "pic5.jpg", alt: "Large moth on a leaf" },
+];
+
+const baseURL = "https://mdn.github.io/shared-assets/images/examples/learn/gallery/";
+
+for (const image of images) {
+const newImage = document.createElement("img");
+newImage.src = `${baseURL}${image.filename}`;
+newImage.alt = image.alt;
+thumbBar.appendChild(newImage);
+//newImage.addEventListener("click", updateDisplayyedImage);
+}
+
+function updateDisplayedImage(e) {
+displayedImage.src = e.target.src;
+displayedImage.alt = e.target.alt;
+}
+/*
+btn.addEventListener("click", () =. {
+if (btn.classList.contains("dark")) {
+overlay.style.backgroundColor("grey");
+} else
+overlay.style.backgroundColor("black");
+}*/
