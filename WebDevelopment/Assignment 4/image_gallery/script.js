1be9da1bbd45ff90b493dc0c84084f931562db72
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-07 21:38:10 -0400

    Part 2 initial commit
    Unmodified files for part 2

---
+++ WebDevelopment/Assignment 4/image_gallery/script.js
@@ -0,0 +1,9 @@
+const displayedImage = document.querySelector(".displayed-img");
+const thumbBar = document.querySelector(".thumb-bar");
+
+const btn = document.querySelector("button");
+const overlay = document.querySelector(".overlay");
+
+const images = [
+{filename: "pic1.jpg", alt: "Closeup of a human eye" }
+]
