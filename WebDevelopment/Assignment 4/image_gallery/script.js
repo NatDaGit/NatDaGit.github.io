1ed3044cd75832115a1b4e400a08d28858e7ec80
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-14 13:38:34 -0400

    Part 2 done
    what i THOUGHT was a bug that i was going to bring up in class today ws just me missing the fact that there was a css sheet, due to the lack of clear tutorial file given by the mdn. I had to take it from the play demo.

--- WebDevelopment/Assignment 4/image_gallery/script.js
+++ WebDevelopment/Assignment 4/image_gallery/script.js
@@ -31,10 +31,14 @@ function updateDisplayedImage(e) {
 displayedImage.src = e.target.src;
 displayedImage.alt = e.target.alt;
 }
-/*
-btn.addEventListener("click", () =. {
-if (btn.classList.contains("dark")) {
-overlay.style.backgroundColor("grey");
-} else
-overlay.style.backgroundColor("black");
-}*/
+
+btn.addEventListener("click", () => {
+  if (btn.classList.contains("dark")) {
+    btn.textContent = "Lighten";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0.5)";
+  } else {
+    btn.textContent = "Darken";
+    overlay.style.backgroundColor = "rgb(0 0 0 / 0)";
+  }
+  btn.classList.toggle("dark");
+});
--- WebDevelopment/Assignment 4/object_building_practice/main.js
+++ WebDevelopment/Assignment 4/object_building_practice/main.js
@@ -5,7 +5,6 @@ const ctx = canvas.getContext("2d");
 
 const width = (canvas.width = window.innerWidth);
 const height = (canvas.height = window.innerHeight);
-
 // function to generate random number
 
 function random(min, max) {
@@ -16,4 +15,22 @@ function random(min, max) {
 
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
+}
+
+class Ball {
+  constructor(x, y, velX, velY, color, size) {
+    this.x = x;
+    this.y = y;
+    this.velX = velX;
+    this.velY = velY;
+    this.color = color;
+    this.size = size;
+  }
+
+  draw() {
+    ctx.beginPath();
+    ctx.fillStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.fill();
+  }
 }
