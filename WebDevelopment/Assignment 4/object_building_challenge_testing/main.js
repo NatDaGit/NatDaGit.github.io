91e1596a59604e3af04f07b637025bc4b9442d5a
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-16 21:06:00 -0400

    Part 4 update 2
    untested, but these three new methods should function. I need to now implement them.

--- WebDevelopment/Assignment 4/object_building_challenge_testing/main.js
+++ WebDevelopment/Assignment 4/object_building_challenge_testing/main.js
@@ -24,7 +24,7 @@ class Shape {
     this.velX = velX;
     this.velY = velY;
   }
- }
+}
 class Ball extends Shape {
   constructor(x, y, velX, velY, color, size) {
     super(x, y, velX, velY);
@@ -32,29 +32,6 @@ class Ball extends Shape {
     this.size = size;
     this.exists = true;
   }
- class EvilCircle extends Shape {
-   super(x, y, 20, 20)
-   this.color = white;
-   this.size = 10;
-
-
-  window.addEventListener("keydown", (e) => {
-  switch (e.key) {
-    case "a":
-      this.x -= this.velX;
-      break;
-    case "d":
-      this.x += this.velX;
-      break;
-    case "w":
-      this.y -= this.velY;
-      break;
-    case "s":
-      this.y += this.velY;
-      break;
-  }
-});
- }
 
   draw() {
     ctx.beginPath();
@@ -93,6 +70,71 @@ class Ball extends Shape {
 
         if (distance < this.size + ball.size) {
           ball.color = this.color = randomRGB();
+        }
+      }
+    }
+  }
+}
+
+class EvilCircle extends Shape {
+constructor(x, y) {
+    super(x, y, velX, velY);
+    this.color = color;
+    this.size = size;
+    this.exists = true;
+
+
+  window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
+}
+draw() {
+    ctx.beginPath();
+    ctx.lineWidth = 3;
+    ctx.strokeStyle = this.color;
+    ctx.arc(this.x, this.y, this.size, 0, 2 * Math.PI);
+    ctx.stroke();
+  }
+update() {
+    if (this.x + this.size >= width) {
+      this.velX-=this.size;
+    }
+
+    if (this.x - this.size <= 0) {
+      this.velX+=this.size;
+    }
+
+    if (this.y + this.size >= height) {
+      this.velY-=this.size);
+    }
+
+    if (this.y - this.size <= 0) {
+      this.velY+=this.size;
+    }
+  }
+collisionDetect() {
+    for (const ball of balls) {
+      if (ball.exists) {
+        const dx = this.x - ball.x;
+        const dy = this.y - ball.y;
+        const distance = Math.sqrt(dx * dx + dy * dy);
+
+        if (distance < this.size + ball.size) {
+          ball.exists = false;
+	  count = count-1;
         }
       }
     }
