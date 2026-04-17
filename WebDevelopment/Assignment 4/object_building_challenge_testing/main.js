8418a3241a19ed3b34f14e0b24c808d84ecef05a
NatDaGit <nathan.hewitt@dcmail.ca>
2026-04-16 20:47:41 -0400

    part 4 progress 1
    made good progress with extending shape and retained functionality, currently nonfunctional, i might have put the extend in the wrong spot

--- WebDevelopment/Assignment 4/object_building_challenge_testing/main.js
+++ WebDevelopment/Assignment 4/object_building_challenge_testing/main.js
@@ -17,16 +17,44 @@ function random(min, max) {
 function randomRGB() {
   return `rgb(${random(0, 255)},${random(0, 255)},${random(0, 255)})`;
 }
-
-class Ball {
-  constructor(x, y, velX, velY, color, size) {
+class Shape {
+  constructor(x, y, velX, velY ){
     this.x = x;
     this.y = y;
     this.velX = velX;
     this.velY = velY;
+  }
+ }
+class Ball extends Shape {
+  constructor(x, y, velX, velY, color, size) {
+    super(x, y, velX, velY);
     this.color = color;
     this.size = size;
+    this.exists = true;
   }
+ class EvilCircle extends Shape {
+   super(x, y, 20, 20)
+   this.color = white;
+   this.size = 10;
+
+
+  window.addEventListener("keydown", (e) => {
+  switch (e.key) {
+    case "a":
+      this.x -= this.velX;
+      break;
+    case "d":
+      this.x += this.velX;
+      break;
+    case "w":
+      this.y -= this.velY;
+      break;
+    case "s":
+      this.y += this.velY;
+      break;
+  }
+});
+ }
 
   draw() {
     ctx.beginPath();
@@ -58,7 +86,7 @@ class Ball {
 
   collisionDetect() {
     for (const ball of balls) {
-      if (!(this === ball)) {
+      if (!(this === ball) && ball.exists) {
         const dx = this.x - ball.x;
         const dy = this.y - ball.y;
         const distance = Math.sqrt(dx * dx + dy * dy);
